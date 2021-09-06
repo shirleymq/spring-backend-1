36e72a33a818893718f3b5e74c69c0aace26dca7
tercere <tercere@labcorp.com>
2021-09-06 15:54:18 -0400

    labo 10 concurrencia

---
+++ src/main/java/com/sales/market/model/AccountAux.java
@@ -0,0 +1,55 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package com.sales.market.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class AccountAux /*extends ModelBase*/ {
+
+    private BigDecimal totalDebit;
+    private BigDecimal totalCredit;
+    private BigDecimal balance;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getTotalDebit() {
+        return totalDebit;
+    }
+
+    public void setTotalDebit(BigDecimal totalDebit) {
+        this.totalDebit = totalDebit;
+    }
+
+    public BigDecimal getTotalCredit() {
+        return totalCredit;
+    }
+
+    public void setTotalCredit(BigDecimal totalCredit) {
+        this.totalCredit = totalCredit;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
