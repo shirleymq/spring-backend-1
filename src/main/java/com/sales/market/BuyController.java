7d7110b8f9893eaca54d2566fca1070df5c73771
tercere <tercere@labcorp.com>
2021-09-07 18:27:13 -0400

    servicios y controladores

---
+++ src/main/java/com/sales/market/BuyController.java
@@ -0,0 +1,28 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package com.sales.market;
+
+import com.sales.market.dto.BuyDto;
+import com.sales.market.model.Buy;
+import com.sales.market.service.BuyService;
+import com.sales.market.service.GenericService;
+import controller.GenericController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/buys")
+public class BuyController extends GenericController<Buy, BuyDto> {
+    private BuyService service;
+
+    public BuyController(BuyService service) {
+        this.service = service;
+    }
+
+    @Override
+    protected GenericService getService() {
+        return service;
+    }
+}
