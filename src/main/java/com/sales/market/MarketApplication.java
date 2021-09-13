38957db375eda5b8b361b223ebd2ae3f9c7e9d3c
tercere <tercere@labcorp.com>
2021-09-13 11:17:41 -0400

    Swagger funciona. Securidad basica funciona. Rutas funcionan

--- src/main/java/com/sales/market/MarketApplication.java
+++ src/main/java/com/sales/market/MarketApplication.java
@@ -2,8 +2,9 @@ package com.sales.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication/*(exclude = {SecurityAutoConfiguration.class})*/
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MarketApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sales/market/controller/BuyController.java
+++ src/main/java/com/sales/market/controller/BuyController.java
@@ -8,6 +8,7 @@ import com.sales.market.dto.BuyDto;
 import com.sales.market.model.Buy;
 import com.sales.market.service.BuyService;
 import com.sales.market.service.GenericService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,14 +30,14 @@ public class BuyController extends GenericController<Buy, BuyDto> {
         super.deleteElement(id);
     }*/
 
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     //@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public String onlyUsers() {
         return "You are have ROLE: USER";
     }
 
-    //    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String onlyAdmins() {
         return "You are have ROLE: ADMIN";
